1424f3909fd1463cdc2d01431e0316030ccb91a7
David Hype <iamsocialhype@gmail.com>
2025-05-30 16:36:26 +0100

    feat: intergrated the api for the createEvents as an admin

---
+++ src/types/Events.ts
@@ -0,0 +1,13 @@
+export type CreateEventType = {
+  name: string;
+  description: string;
+  address: string;
+  price: string;
+  total_ticket: string;
+  date: string;
+  time: string;
+  age_range: string;
+  instruction: string;
+  category: string;
+  isPublished: string;
+};
