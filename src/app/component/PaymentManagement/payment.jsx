d1833829f38eb70b98889bdf2a804aab6af05f97
David Hype <iamsocialhype@gmail.com>
2025-08-20 08:51:21 +0100

    yupp

--- src/app/component/PaymentManagement/payment.jsx
+++ src/app/component/PaymentManagement/payment.jsx
@@ -200,8 +200,8 @@ export default function Payment() {
                                     placeholder={`Search for ${activeTab.toLowerCase()}...`}
                                     value={searchTerm}
                                     onChange={handleSearchChange}
-                                    className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                                <Search className="absolute left-3 top-2.5 text-gray-400 w-5 h-5" />
+                                    className="w-full pl-10 pr-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                                <Search className="absolute left-3 top-2.5 text-black w-5 h-5" />
                             </div>
                             <div className="relative w-full md:w-auto">
                                 <button
