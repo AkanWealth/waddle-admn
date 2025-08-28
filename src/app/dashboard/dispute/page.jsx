1a5bf4bb21ee7b7c72176d5ce0e0e17ceafa25d4
David Hype <iamsocialhype@gmail.com>
2025-08-28 16:45:18 +0100

    feat: added toast on dispute

--- src/app/dashboard/dispute/page.jsx
+++ src/app/dashboard/dispute/page.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 // import EventManagement from "../../component/EventManagement/Event";
 import Dispute from "../../component/Dispute/dispute";
+import { ToastContext } from "@/context/toast";
 
 
 export default function Page() {
   return (
+    <ToastContext>
       <Dispute />
-
+    </ToastContext>
   );
 }
